a92f2534b838bdeae86166f1c46d26376e7c4c7e
nkmwambs <nkmwambs@gmail.com>
2019-09-24 14:12:03 +0300

    Added PHPMailer

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,6 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+// Load Composer's autoloader
+require 'vendor/autoload.php';
+		// Import PHPMailer classes into the global namespace
+		// These must be at the top of your script, not inside a function
+		use PHPMailer\PHPMailer\PHPMailer;
+		use PHPMailer\PHPMailer\Exception;
+
 class Welcome extends CI_Controller {
 
 	/**
@@ -20,6 +27,46 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
+
+		// Instantiation and passing `true` enables exceptions
+		$mail = new PHPMailer(true);
+
+		try {
+		    //Server settings
+		    $mail->SMTPDebug = 2;                                       // Enable verbose debug output
+		    $mail->isSMTP();                                            // Set mailer to use SMTP
+		    $mail->Host       = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
+		    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+		    $mail->Username   = 'user@example.com';                     // SMTP username
+		    $mail->Password   = 'secret';                               // SMTP password
+		    //$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption, `PHPMailer::ENCRYPTION_SMTPS` also accepted
+		    $mail->Port       = 587;                                    // TCP port to connect to
+
+		    //Recipients
+		    $mail->setFrom('from@example.com', 'Mailer');
+		    $mail->addAddress('joe@example.net', 'Joe User');     // Add a recipient
+		    $mail->addAddress('ellen@example.com');               // Name is optional
+		    $mail->addReplyTo('info@example.com', 'Information');
+		    $mail->addCC('cc@example.com');
+		    $mail->addBCC('bcc@example.com');
+
+		    // Attachments
+		    $mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
+		    $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
+
+		    // Content
+		    $mail->isHTML(true);                                  // Set email format to HTML
+		    $mail->Subject = 'Here is the subject';
+		    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+		    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+		    $mail->send();
+		    echo 'Message has been sent';
+		} catch (Exception $e) {
+		    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+		}
+
+
 		$this->load->view('welcome_message');
 	}
 }
